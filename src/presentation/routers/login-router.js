a16d7a437e6f99a07ed66854f90a581f24bdd776
Caio César Roma <caioroma7@gmail.com>
2023-05-30 22:55:35 -0300

    Testando retorno de 200

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -22,8 +22,11 @@ module.exports = class LoginRouter {
             return HttpResponse.badRequest('password');
         }
 
-        this.authUseCase.auth(email, password);
+        const accessToken = this.authUseCase.auth(email, password);
 
-        return HttpResponse.unauthorizedError();
+        if (!accessToken)
+            return HttpResponse.unauthorizedError();
+
+        return HttpResponse.ok();
     }
 }
